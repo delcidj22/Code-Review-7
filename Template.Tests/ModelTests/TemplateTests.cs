ad403013da24471da8fec040dedf80b87ed99412
delcidj22 <92245521+delcidj22@users.noreply.github.com>
2022-02-12 13:50:13 -0800

    Initial commit

---
+++ Template.Tests/ModelTests/TemplateTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Template.Models; //Change name based on project name
+using System;
+
+namespace Template.Tests // Change Name 
+{
+  //Project Code
+}
---
+++ Template/Models/Template.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+namespace Template.Modles //Name needs to change depending on project name
+{
+  //Project Code 
+}
