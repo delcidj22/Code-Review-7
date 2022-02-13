8193394aee209707ed2af101a45b1c3e99cce84c
delcidj22 <jdelcdid23@gmail.com>
2022-02-12 23:49:25 -0800

    Add Tests

--- Bakery.Tests/ModelTests/Bakery.Tests.cs
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -1,9 +1,87 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using Template.Models; //Change name based on project name
+using Bakery.Models; 
 using System;
 
-namespace Template.Tests // Change Name 
+namespace Bakery.Tests 
 {
-  //Project Code
-}
+  [TestClass]
+  public class BakeryTests
+  {
+
+  [TestMethod]
+
+  public void BreadConstructor_CreatesAnInstanceOfBread_Bread()
+  {
+    Bread newBread = new Bread(1);
+    Assert.AreEqual(typeof(Bread), newBread.GetType());
+  }
+
+  [TestMethod]
+  public void SetNumber_SetNumberOfLoaves_Int()
+  {
+  int NumberOfLoaves = 1;
+  Bread newBread = new Bread(NumberOfLoaves);
+  Assert.AreEqual(1, NumberOfLoaves);
+  }
+
+  [TestMethod]
+
+  public void SetPrice_SetPriceOfOneLoaf_Int()
+  {
+    int NumberOfLoaves = 1;
+    Bread newBread = new Bread(NumberOfLoaves);
+    newBread.SetPrice();
+    Assert.AreEqual(5, newBread.SetPrice());
+  }
+
+  [TestMethod]
+
+  public void SetPrice_SetDiscountedPrice_Int()
+  {
+    int NumberOfLoaves = 3;
+    Bread newBread = new Bread(NumberOfLoaves);
+    newBread.SetPrice();
+    Assert.AreEqual(10, newBread.SetPrice());
+  }
+
+  [TestMethod]
+
+  public void PastryConstructor_CreatesAnInstanceOfPastries_Pastry()
+  {
+    Pastry newPastry = new Pastry(1);
+    Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+  }
+
+  [TestMethod]
+  
+  public void SetNumber_SetNumberOfPastries_Int()
+  {
+    int NumberOfPastries = 1;
+    Pastry newPastry = new Pastry(NumberOfPastries);
+    Assert.AreEqual(1, NumberOfPastries);
+  }
+
+  [TestMethod]
+  
+  
+  public void SetPrice_SetPriceOfOnePastry_Int()
+  {
+    int NumberOfPastries = 1;
+    Pastry newPastry = new Pastry(NumberOfPastries);
+    newPastry.SetPrice();
+    Assert.AreEqual(2, newPastry.SetPrice());
+  }
+
+  [TestMethod]
+
+  public void SetPrice_SetPriceOfGreaterNumberOfPastries_Int()
+  {
+    int NumberOfPastries = 6;
+    Pastry newPastry = new Pastry(NumberOfPastries);
+    newPastry.SetPrice();
+    Assert.AreEqual(10, newPastry.SetPrice());
+  }
+  }
+} 
+  
